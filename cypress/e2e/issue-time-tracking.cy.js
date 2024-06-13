f6b9760086c47715e1c615daf58cb6434266fa6c
dimplesha29 <dimple.sha29@gmail.com>
2024-06-14 04:01:10 +0530

    My commets about the commit

---
+++ cypress/e2e/issue-time-tracking.cy.js
@@ -0,0 +1,48 @@
+describe("Time estimation functionality", () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project`)
+      .then((url) => {
+        cy.visit(url + "/board");
+        cy.get('[data-testid="board-list:backlog"]')
+          .should("be.visible")
+          .and("have.length", "1")
+          .within(() => {
+            // Assert that this list contains 4 issues and first element with tag p has specified text
+            cy.get('[data-testid="list-issue"]')
+              .first()
+              .should("contain", "This is an issue of type: Task.");
+            cy.get('[data-testid="list-issue"]').first().click();
+          });
+      });
+  });
+
+  const getIssueDetailsModal = () =>
+    cy.get('[data-testid="Modal:issue-details"]');
+
+  const originalestimatehourselector = '[placeholder="Number"]';
+  //const timetrackingselector = ".sc-dxgOiQ HrhWu";
+  const addtime = "10";
+  const updatedestimatedTime = "15";
+
+  it("Should add, update and remove estimated time in the issue", () => {
+    //Add estimated time
+    cy.get(originalestimatehourselector)
+      .clear()
+      .type(addtime)
+      .should("have.value", addtime);
+    //cy.get(originalestimatehourselector).should("contain", addtime);
+
+    //Add estimated updated time
+    cy.get(originalestimatehourselector)
+      .clear()
+      .type(updatedestimatedTime)
+      .should("have.value", updatedestimatedTime);
+    //cy.get(timetrackingselector).should("contain", updatedestimatedTime);
+
+    // Remove estimated time
+    cy.get(originalestimatehourselector).clear().should("have.value", "");
+    //cy.get(timetrackingselector).should("not.have.value");
+  });
+});
